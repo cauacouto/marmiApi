99ddb1b7545ff05ad07497f7e4e1c54e6e55ae66
caua <cauacouto022@gmail.com>
2026-02-13 00:25:28 -0300

    fish commit

---
+++ src/main/java/com/coutodev/marmiMvp/MarmiMvpApplication.java
@@ -0,0 +1,13 @@
+package com.coutodev.marmiMvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmiMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmiMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coutodev/marmiMvp/MarmiMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutodev.marmiMvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmiMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
