9be3dc1be42283193f8b440e2df18a682202af39
caua <cauacouto022@gmail.com>
2026-02-21 17:01:12 -0300

    feat: reposiotorys

---
+++ src/main/java/com/coutodev/marmiMvp/domain/Pedido/PedidoRepository.java
@@ -0,0 +1,6 @@
+package com.coutodev.marmiMvp.domain.Pedido;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository  extends JpaRepository<Pedido,Long> {
+}
---
+++ src/main/java/com/coutodev/marmiMvp/domain/Refeicoes/RefeicaoRepository.java
@@ -0,0 +1,6 @@
+package com.coutodev.marmiMvp.domain.Refeicoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefeicaoRepository extends JpaRepository<Refeicao,Long> {
+}
