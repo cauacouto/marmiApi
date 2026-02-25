3f911433bd985b04df5c4a01f06a1fcf6f0c29f8
caua <cauacouto022@gmail.com>
2026-02-25 19:36:28 -0300

    refactor: simplify CriarRefeicaoDto by removing unused fields

--- src/main/java/com/coutodev/marmiMvp/DTO/CriarRefeicaoDto.java
+++ src/main/java/com/coutodev/marmiMvp/DTO/CriarRefeicaoDto.java
@@ -1,14 +1,12 @@
 package com.coutodev.marmiMvp.DTO;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record CriarRefeicaoDto(String nome,
                                String descricao,
                                Double calorias,
                                Double proteinas,
                                Double gorduras,
-                               BigDecimal preco,
-                               Boolean ativo,
-                               UUID parceiroId) {
+                               BigDecimal preco
+                               ) {
 }
