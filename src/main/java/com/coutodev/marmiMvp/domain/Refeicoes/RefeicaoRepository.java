637288e5ab81c07a0cbfa941bb0e985c3143e76d
caua <cauacouto022@gmail.com>
2026-03-02 23:50:26 -0300

    feat: add metodos to find Refeicao by parceiroId and parceiroTipoCozinha

--- src/main/java/com/coutodev/marmiMvp/domain/Refeicoes/RefeicaoRepository.java
+++ src/main/java/com/coutodev/marmiMvp/domain/Refeicoes/RefeicaoRepository.java
@@ -1,6 +1,15 @@
 package com.coutodev.marmiMvp.domain.Refeicoes;
 
+import com.coutodev.marmiMvp.enums.TipoCozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface RefeicaoRepository extends JpaRepository<Refeicao,Long> {
+
+
+    List<Refeicao> findByParceiroId(UUID parceiroId);
+
+ List<Refeicao>  findByParceiroTipoCozinha(TipoCozinha tipoCozinha);
 }
