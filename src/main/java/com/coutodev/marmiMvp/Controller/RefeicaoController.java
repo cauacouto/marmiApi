7dcf57c88b30aef565000659ee8dea4b83cec0e7
caua <cauacouto022@gmail.com>
2026-03-02 23:49:59 -0300

    feat: add listar method and update criarRefeicao to use @RequestParam

--- src/main/java/com/coutodev/marmiMvp/Controller/RefeicaoController.java
+++ src/main/java/com/coutodev/marmiMvp/Controller/RefeicaoController.java
@@ -3,29 +3,34 @@ package com.coutodev.marmiMvp.Controller;
 import com.coutodev.marmiMvp.DTO.CriarRefeicaoDto;
 import com.coutodev.marmiMvp.DTO.RefeicaoResponseDto;
 import com.coutodev.marmiMvp.domain.Refeicoes.RefeicaoService;
+import com.coutodev.marmiMvp.enums.TipoCozinha;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/refeicao")
 public class RefeicaoController {
 
-private final RefeicaoService refeicaoService;
+    private final RefeicaoService refeicaoService;
 
 
     public RefeicaoController(RefeicaoService refeicaoService) {
         this.refeicaoService = refeicaoService;
 
     }
-@PostMapping
-    public ResponseEntity<RefeicaoResponseDto> criarRefeicao(@RequestBody CriarRefeicaoDto dto,UUID id){
-       RefeicaoResponseDto response = refeicaoService.criarRefeicao(dto,id);
-       return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+    @PostMapping
+    public ResponseEntity<RefeicaoResponseDto> criarRefeicao(@RequestBody CriarRefeicaoDto dto, @RequestParam UUID id) {
+        RefeicaoResponseDto response = refeicaoService.criarRefeicao(dto, id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping
+    public ResponseEntity<List<RefeicaoResponseDto>> listar(@RequestParam(required = false) TipoCozinha tipoCozinha){
+        return ResponseEntity.ok(refeicaoService.listar(tipoCozinha));
     }
 }
+
