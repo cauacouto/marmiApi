3c5944c7260e394d270b605b917926f5d479c928
caua <cauacouto022@gmail.com>
2026-03-02 23:49:15 -0300

    feat: add metodos to list

--- src/main/java/com/coutodev/marmiMvp/domain/Refeicoes/RefeicaoService.java
+++ src/main/java/com/coutodev/marmiMvp/domain/Refeicoes/RefeicaoService.java
@@ -6,8 +6,10 @@ import com.coutodev.marmiMvp.Exception.RegraNegocioException;
 import com.coutodev.marmiMvp.Mapper.RefeicaoMapper;
 import com.coutodev.marmiMvp.domain.Parceiros.Parceiro;
 import com.coutodev.marmiMvp.domain.Parceiros.ParceiroRepository;
+import com.coutodev.marmiMvp.enums.TipoCozinha;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,7 +17,7 @@ public class RefeicaoService {
 
     private final ParceiroRepository parceiroRepository;
     private final RefeicaoRepository refeicaoRepository;
-    private final RefeicaoMapper  refeicaoMapper;
+    private final RefeicaoMapper refeicaoMapper;
 
     public RefeicaoService(ParceiroRepository parceiroRepository, RefeicaoRepository refeicaoRepository, RefeicaoMapper refeicaoMapper) {
         this.parceiroRepository = parceiroRepository;
@@ -23,14 +25,37 @@ public class RefeicaoService {
         this.refeicaoMapper = refeicaoMapper;
     }
 
-    public RefeicaoResponseDto criarRefeicao(CriarRefeicaoDto dto, UUID id){
+    public RefeicaoResponseDto criarRefeicao(CriarRefeicaoDto dto, UUID id) {
         Parceiro parceiro = parceiroRepository.findById(id)
-                .orElseThrow(()-> new RegraNegocioException("parceiro não encontrado"));
+                .orElseThrow(() -> new RegraNegocioException("parceiro não encontrado"));
 
         Refeicao refeicao = refeicaoMapper.toEntity(dto);
         refeicao.setParceiro(parceiro);
         Refeicao refeicaoSalva = refeicaoRepository.save(refeicao);
 
         return refeicaoMapper.toResponseDto(refeicaoSalva);
+    }
+
+
+    public List<RefeicaoResponseDto> listarRefeicoesPorParceiro(UUID parceiroId) {
+        parceiroRepository.findById(parceiroId)
+                .orElseThrow(() -> new RegraNegocioException("parceiro não encontrado"));
+
+        return refeicaoRepository
+                .findByParceiroId(parceiroId)
+                .stream()
+                .map(refeicaoMapper::toResponseDto)
+                .toList();
+    }
+
+
+    public List<RefeicaoResponseDto> listar(TipoCozinha tipoCozinha) {
+       List<Refeicao> refeicao =  (tipoCozinha != null)
+            ? refeicaoRepository.findByParceiroTipoCozinha(tipoCozinha)
+               : refeicaoRepository.findAll();
+
+       return refeicao.stream()
+               .map(refeicaoMapper::toResponseDto)
+               .toList();
     }
 }
